cd9f58ea081a1fc0b4379e568bd418c3423c7eff
diegogirao <diegoramiro25@gmail.com>
2023-10-21 16:46:12 +0100

    feat created examples of arrays using primitive

---
+++ src/br/com/fiap/arrays/Arrays.java
@@ -0,0 +1,22 @@
+package br.com.fiap.arrays;
+
+public class Arrays {
+    public static void main(String[] args) {
+//        float[] values = new float[5];
+//
+//        values[0] = 10;
+//        values[1] = 20;
+//        values[2] = 30;
+//        values[3] = 40;
+//        values[4] = 50;
+//
+//        System.out.println("Valor do indíce 1: " + values[1]);
+//        System.out.println("Valor do indíce 3: " + values[3]);
+        float[] value = {10, 20, 30, 40, 50};
+        float[] value2 = new float[]{15, 25, 35, 45, 55};
+
+        System.out.println("O valor no indice 4: " + value[4]);
+        System.out.println("O valor no indice 4: " + value2[4]);
+
+    }
+}
