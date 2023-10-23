99fa0ae763a1d74cf500b8eb86059ae8fb0cc92e
diegogirao <diegoramiro25@gmail.com>
2023-10-23 16:52:53 +0100

    feat second example, set string and hashset

--- src/br/com/fiap/collection/ArrayList.java
+++ src/br/com/fiap/collection/ArrayList.java
@@ -6,22 +6,34 @@ import java.util.Set;
 
 public class ArrayList {
     public static void main(String[] args) {
+        //  Exemplo2
+        Set<String> basket = new HashSet<String>();
+
+        System.out.println(basket.isEmpty());
+        basket.add("Uva");
+        basket.add("Uva");
+        basket.add("uva");
+        System.out.println(basket.isEmpty());
+
+        System.out.println(basket.size());
+        System.out.println(basket);
+
         // Exemplo 1
-        List<String> chart = new java.util.ArrayList<String>();
-
-        chart.add("Vikings");
-        chart.add("The Walking Dead");
-        chart.add("Vikings");
-
-        System.out.println(chart.contains("Vikings"));
-        System.out.println(chart.indexOf("Vikings"));
-        System.out.println(chart.get(chart.indexOf("Vikings")));
-        System.out.println(chart.lastIndexOf("Vikings"));
-
-        chart.remove(chart.indexOf("Vikings"));
-        System.out.println(chart.get(0));
-
-        chart.clear();
-        System.out.println(chart.isEmpty());
+//        List<String> chart = new java.util.ArrayList<String>();
+//
+//        chart.add("Vikings");
+//        chart.add("The Walking Dead");
+//        chart.add("Vikings");
+//
+//        System.out.println(chart.contains("Vikings"));
+//        System.out.println(chart.indexOf("Vikings"));
+//        System.out.println(chart.get(chart.indexOf("Vikings")));
+//        System.out.println(chart.lastIndexOf("Vikings"));
+//
+//        chart.remove(chart.indexOf("Vikings"));
+//        System.out.println(chart.get(0));
+//
+//        chart.clear();
+//        System.out.println(chart.isEmpty());
     }
 }
