21349519de4ce6a2283b9b85a3a498e505a3a62b
diegogirao <diegoramiro25@gmail.com>
2023-10-23 16:51:30 +0100

    feat first example, used List

--- src/br/com/fiap/collection/ArrayList.java
+++ src/br/com/fiap/collection/ArrayList.java
@@ -1,7 +1,27 @@
 package br.com.fiap.collection;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class ArrayList {
     public static void main(String[] args) {
-        
+        // Exemplo 1
+        List<String> chart = new java.util.ArrayList<String>();
+
+        chart.add("Vikings");
+        chart.add("The Walking Dead");
+        chart.add("Vikings");
+
+        System.out.println(chart.contains("Vikings"));
+        System.out.println(chart.indexOf("Vikings"));
+        System.out.println(chart.get(chart.indexOf("Vikings")));
+        System.out.println(chart.lastIndexOf("Vikings"));
+
+        chart.remove(chart.indexOf("Vikings"));
+        System.out.println(chart.get(0));
+
+        chart.clear();
+        System.out.println(chart.isEmpty());
     }
 }
